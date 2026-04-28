e841a04b8dd26b7cbc8eac88c8b60da9c841c40c
张鑫 <1964362087@qq.com>
2026-04-28 10:56:34 +0800

    feat: SSE 流式生成题目

--- AI_Auto_Q_backend/src/test/java/ZhiPuAiTest.java
+++ AI_Auto_Q_backend/src/test/java/ZhiPuAiTest.java
@@ -6,11 +6,14 @@ import com.zhipu.oapi.service.v4.model.ChatCompletionRequest;
 import com.zhipu.oapi.service.v4.model.ChatMessage;
 import com.zhipu.oapi.service.v4.model.ChatMessageRole;
 import com.zhipu.oapi.service.v4.model.ModelApiResponse;
+import io.reactivex.Flowable;
+import io.reactivex.schedulers.Schedulers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -37,5 +40,22 @@ public class ZhiPuAiTest {
         // 调用
         ModelApiResponse invokeModelApiResp = client.invokeModelApi(chatCompletionRequest);
         System.out.println("model output:" + invokeModelApiResp.getMsg());
+    }
+
+
+    @Test
+    void rxJavaDemo() throws InterruptedException {
+        // 创建一个流，每秒发射一个递增的整数（数据流变化）
+        Flowable<Long> flowable = Flowable.interval(1, TimeUnit.SECONDS)
+                .map(i -> i + 1)
+                .subscribeOn(Schedulers.io()); // 指定创建流的线程池
+
+        // 订阅 Flowable 流，并打印每个接受到的数字
+        flowable.observeOn(Schedulers.io())
+                .doOnNext(item -> System.out.println(item.toString()))
+                .subscribe();
+
+        // 让主线程睡眠，以便观察输出
+        Thread.sleep(10000L);
     }
 }
